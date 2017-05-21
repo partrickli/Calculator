d8044c4d120f923d0bde90f4b5404995dbe87829
partrick.li <partrick.li@gmail.com>
2017-05-21 17:12:32 +0800

    calculator key board settled

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        if let digit = sender.currentTitle {
+            print("\(digit) was pressed")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
