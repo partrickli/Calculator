60d11574c7ad3869a8c5f070b9391c24fd126723
partrick.li <partrick.li@gmail.com>
2017-05-22 15:56:00 +0800

    basic calculation and displayValue tracking label displayed value

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,10 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // track value of display label
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBOutlet weak var display: UILabel!
+    
     @IBAction func touchDigit(_ sender: UIButton) {
-        if let digit = sender.currentTitle {
-            print("\(digit) was pressed")
+        
+        if let digit = sender.currentTitle, let displayText = display.text {
+            if userIsInTheMiddleOfTyping {
+                display.text = displayText + digit
+            } else {
+                display.text = digit
+                userIsInTheMiddleOfTyping = true
+            }
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
         }
     }
 }
+
 
